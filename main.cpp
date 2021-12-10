12ba460128769b5396dad21f2f4a10ae4e5382e7
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-10 10:32:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    char a[10][20];
+    for(int i=0;i<10;i++){
+       for(int j=0;j<20;j++){
+           a[i][j]=0;
+       }
+    }
+    for(int i=0;i<10;i++){
+        cin >> a[i];
+    }
+    char b[20];
+    int p;
+    for(int i=0;i<20;i++){
+        b[i]=0;
+    }
+    cin >> b;
+    if(funzione (a,b,p)){
+        cout << p;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
