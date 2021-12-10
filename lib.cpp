5417f31669fec3d1d597e8001d6aa43c819200e6
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-10 10:31:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+//
+// Created by Andrea Fuselli on 10/12/21.
+//
+
+#include "lib.h"
+
+bool funzione (char a[10][20], char b[20], int &p){
+    int k=0;
+    for(int i=0;i<10;i++){
+        k=0;
+        for(int j=0;j<20;j++){
+            if(a[i][j]==b[j]){
+                k++;
+                if(k==20){
+                    p=i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 
