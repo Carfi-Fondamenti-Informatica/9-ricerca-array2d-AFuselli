9b060a279ae9e3084119ac2297f55d8e1b1e1a3f
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-10 10:31:51 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Andrea Fuselli on 10/12/21.
+//
+
+#ifndef COMPITO9_RICERCAARRAY2D_LIB_H
+#define COMPITO9_RICERCAARRAY2D_LIB_H
+
+bool funzione(char [10][20], char [20], int &);
+
+#endif //COMPITO9_RICERCAARRAY2D_LIB_H
 
